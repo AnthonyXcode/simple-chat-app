a59753608199d9e6b3e4e54c7cec3d4864f43788
anthony chan <anthonyxcode@gmial.com>
2019-12-03 23:30:40 +0800

    add message bubble

--- Container/chat.tsx
+++ Container/chat.tsx
@@ -1,11 +1,103 @@
 import React, { Component } from 'react'
-import {Text, View} from 'react-native'
+import { Text, View, FlatList, Button, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
+import * as firebase from 'firebase'
+import { pathOr, append, sort } from 'ramda'
+
+const styles = StyleSheet.create({
+  messageContainer: {
+    position: 'absolute', left: 0, right: 0, bottom: 0, height: '64px', borderTopColor: 'grey', borderTopWidth: 1, flexDirection: 'row', alignItems: 'center'
+  },
+  input: {
+    flex: 1, marginLeft: 8, marginRight: 8
+  },
+  message: {
+    borderWidth: 1, borderColor: 'grey', borderRadius: 8, padding: 8, width: '80px', textAlign: 'center'
+  }
+})
+
+interface Message {
+  user: string,
+  time: Number,
+  message: string
+}
+interface State {
+  message?: string,
+  allmessages?: [Message]
+}
 
 class chat extends Component {
-  render() {
+  state: State
+  messageRef = firebase.database().ref('message')
+
+  componentDidMount() {
+    this.messageRef.on('value', snapshot => {
+      const values = snapshot.val()
+
+      let initMessages: Message[]
+      initMessages = []
+      const messages: [Message] = Object.keys(values).reduce((acc, current, index, arr) => {
+        const value = values[current]
+        const message = value['message']
+        const user = value['user']
+        const time = value['time']
+        if (message && user && time) {
+          return append({ message, time, user }, acc)
+        }
+        return acc
+      }, initMessages)
+      this.setState({
+        allmessages: sort((lhs, rhs) => lhs.time > rhs.time, messages)
+      })
+    })
+  }
+
+  onTextChange = (text) => {
+    this.setState({
+      message: text
+    })
+  }
+
+  onSendPress = () => {
+    const { message } = this.state
+    if (message) {
+      this.messageRef.push().set({
+        message,
+        time: new Date().getTime(),
+        user: firebase.auth().currentUser.email
+      })
+    }
+  }
+
+  renderItem = ({ item }: { item: Message }) => {
+    const myEmail = firebase.auth().currentUser.email
+    const isMyMessage = myEmail === item.user
     return (
-      <View>
-        <Text>Welcome to chat!</Text>
+      <View style={{ flexDirection: isMyMessage ? 'row-reverse' : 'row' }}>
+        <View style={{ margin: 8, padding: 8, borderWidth: 1, borderColor: 'grey', borderRadius: 8 }}>
+          <Text>{item.user}:</Text>
+          <Text>{item.message}</Text>
+          <Text>time:{item.time}</Text>
+        </View>
+      </View>
+    )
+  }
+
+  render() {
+    const messages: [Message] = pathOr([], ['allmessages'], this.state)
+    return (
+      <View style={{ flex: 1 }}>
+        <FlatList
+          style={{ flex: 1, marginBottom: '64px' }}
+          data={messages}
+          renderItem={this.renderItem}
+          scrollEnabled
+        />
+        <View style={styles.messageContainer}>
+          <TextInput style={styles.input} placeholder={'Type message here!'} onChangeText={this.onTextChange} />
+          <TouchableOpacity style={{ marginRight: 8 }} onPress={this.onSendPress}>
+            <Text style={styles.message}>Send</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
--- Container/index.tsx
+++ Container/index.tsx
@@ -102,7 +102,11 @@ class index extends Component {
 
   render() {
     if (this.state.isLoggedIn) {
-      return (<Chat/>)
+      return (
+        <View style={{position: 'absolute', left: 0, right: 0, bottom: 0, top: 0}}>
+          <Chat/>
+        </View>
+    )
     } else {
       return this.renderLogin()
     }
